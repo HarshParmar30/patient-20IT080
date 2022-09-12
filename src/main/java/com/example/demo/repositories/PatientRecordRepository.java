a5eef3645dbb0659d375b8b2fde5dca5ae87824f
HarshParmar30 <89096443+HarshParmar30@users.noreply.github.com>
2022-09-12 13:32:15 +0530

    Patient Repository

--- src/main/java/com/example/demo/repositories/PatientRecordRepository.java
+++ src/main/java/com/example/demo/repositories/PatientRecordRepository.java
@@ -4,5 +4,6 @@ import com.example.demo.model.PatientRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRecordRepository extends JpaRepository<PatientRecord, Long> {
+  
 
 }
