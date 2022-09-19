421fbbc7d9a639b57ba5c2221bbf496934034fb3
HarshParmar30 <89096443+HarshParmar30@users.noreply.github.com>
2022-09-19 13:11:16 +0530

    Backend - errors

--- src/main/java/com/example/demo/errors/InvalidRequestException.java
+++ src/main/java/com/example/demo/errors/InvalidRequestException.java
@@ -7,5 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class InvalidRequestException extends RuntimeException {
     public InvalidRequestException(String s) {
         super(s);
+        
     }
 }
